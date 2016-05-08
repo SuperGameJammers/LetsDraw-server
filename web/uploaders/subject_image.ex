f651e69d033aa6fd8e554fef8724f4bdfbf034ce
Zura Guerra <guerrazuramx@gmail.com>
2016-05-08 05:56:31 -0500

    Save subjects in S3 and return public url

---
+++ web/uploaders/subject_image.ex
@@ -0,0 +1,36 @@
+defmodule Habanero.SubjectImage do
+  use Arc.Definition
+
+  # Include ecto support (requires package arc_ecto installed):
+  use Arc.Ecto.Definition
+
+  @versions [:original]
+
+  # To add a thumbnail version:
+  # @versions [:original, :thumb]
+
+  # Whitelist file extensions:
+  def validate({file, _}) do
+     ~w(.jpg .jpeg .gif .png) |> Enum.member?(Path.extname(file.file_name))
+  end
+
+  # Define a thumbnail transformation:
+  # def transform(:original, _) do
+  #   {:convert, "-gravity Center -resize 250x250^ -crop 250x250+0+0\! -limit area 10MB -limit disk 100MB -format png"}
+  # end
+
+  # Override the persisted filenames:
+  def filename(version, {file, scope}) do
+    "#{scope.name}"
+  end
+
+  # Override the storage directory:
+  def storage_dir(version, {file, scope}) do
+    "subjects"
+  end
+
+  # Provide a default URL if there hasn't been a file uploaded
+  # def default_url(version, scope) do
+  #   "/images/avatars/default_#{version}.png"
+  # end
+end
--- web/views/subject_view.ex
+++ web/views/subject_view.ex
@@ -1,5 +1,6 @@
 defmodule Habanero.SubjectView do
   use Habanero.Web, :view
+  alias Habanero.SubjectImage
 
   def render("index.json", %{subjects: subjects}) do
     %{data: render_many(subjects, Habanero.SubjectView, "subject.json")}
@@ -12,7 +13,7 @@ defmodule Habanero.SubjectView do
   def render("subject.json", %{subject: subject}) do
     %{id: subject.id,
       name: subject.name,
-      img_url: subject.img_url,
+      img_url: SubjectImage.url({subject.img_url, subject}),
       complexity: subject.complexity}
   end
 end
