2197982e6434cbeae09fa4d7091506a285c63017
Dan Amador <danamadorpe@gmail.com>
2016-05-08 01:23:39 -0500

    Added Python scripts

---
+++ lib/machine_learning_py/smart_script.py
@@ -0,0 +1,34 @@
+import io
+import sys
+import urllib
+from itertools import izip
+
+from Pillow import Image
+from havenondemand.hodclient import *
+from scipy import sum
+
+def compare(img1, img2):
+    pairs = izip(img1.getdata(), img2.getdata())
+    if len(img1.getbands()) == 1:
+        dif = sum(abs(p1 - p2) for p1, p2 in pairs)
+    else:
+        dif = sum(abs(c1 - c2) for p1, p2 in pairs for c1, c2 in zip(p1, p2))
+    ncomponents = img1.size[0] * img2.size[1] * 3
+    return (dif / 256.0 * 100) / ncomponents
+
+
+def main():
+    if (len(sys.argv) < 3 or sys.argv[1] == "learn"):
+
+    else:
+        file1, file2 = sys.argv[1:1 + 2]
+        img1 = Image.open(io.BytesIO(urllib.urlopen(file1).read()))
+        img2 = Image.open(io.BytesIO(urllib.urlopen(file2).read()))
+        percentage = compare(img1, img2)
+        return percentage
+
+
+if __name__ == "__main__":
+    main()
+
+client = HODClient("d028225e-f3d7-49ce-b431-c2d769602f1f", version="v1")
